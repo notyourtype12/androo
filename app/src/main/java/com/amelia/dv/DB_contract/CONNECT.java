4bc7f7de5189a002de65703654cec1f742f80433
developer <rikaaarika37@gmail.com>
2024-11-27 16:09:19 +0700

    nama

--- app/src/main/java/com/amelia/dv/DB_contract/CONNECT.java
+++ app/src/main/java/com/amelia/dv/DB_contract/CONNECT.java
@@ -1,7 +1,7 @@
 package com.amelia.dv.DB_contract;
 
 public class CONNECT {
-    public static String ip = "192.168.1.12";
+    public static String ip = "192.168.1.4";
 
     public static final String urlLogin = "http://" + ip + "/CRUDVolley/CRUDVolley/api-login.php";
 }
